91be2ea5cd7ba15fb2d86427be4fa68f249e9d6f
Kevin Lin <kevinslin8@gmail.com>
2012-03-07 17:47:19 -0600

    Kernel initialization
    Details:
    - initialize page frames
    - kind of initialize page table entries

--- src/yalnix.c
+++ src/yalnix.c
@@ -1,20 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include <comp421/yalnix.h>
 #include <comp421/hardware.h>
 
 static int **interrupt_vector_table[TRAP_VECTOR_SIZE];
+
+
 typedef struct {
-  int dummy;
-} free_page_frames;
-typedef struct {
-  int dummy;
-} page_table;
+  int free;
+} page_frames;
+
+
+
+/*
+ * Idle process
+ */
+void process_idle() {
+  for (;;){
+    Pause();
+    printf("idling...\n");
+  }
+}
 
 void KernelStart(ExceptionStackFrame *frame, unsigned int pmem_size, void *orig_brk, char **cmd_args) {
   /* Init */
   int i;
+  int num_frames;
+  int num_pages;
+  /* Initialize constants */
+  num_frames = pmem_size / PAGESIZE;
+  assert(num_frames > 0); // silly...
+  num_pages = VMEM_REGION_SIZE / PAGESIZE;
+  assert(num_pages > 0); // silly...
+
+  /* More constants */
+  page_frames frames[num_frames];
+  pte page_table1[num_pages];
+  pte page_table2[num_pages];
+
   /* Initialize interrupt vector table */
   for (i=0; i < TRAP_VECTOR_SIZE; i++) {
     interrupt_vector_table[i] = NULL;
@@ -22,19 +47,27 @@ void KernelStart(ExceptionStackFrame *frame, unsigned int pmem_size, void *orig_
   /* Point REG_VECTOR_BASE at interupt vector table */
   WriteRegister( REG_VECTOR_BASE, (RCS421RegVal) &interrupt_vector_table);
   /* Keep track of free page frames */
-  free_page_frames free_frames = { 1 };
+
+  for (i=0; i<num_frames; i++) {
+    frames[i].free = 0;
+  }
   /* Initial page tables for region 0 and region 1 */
-  page_table p_reg0;
-  page_table p_reg1;
-  WriteRegister( REG_PTR0, (RCS421RegVal) &p_reg0);
-  WriteRegister( REG_PTR1, (RCS421RegVal) &p_reg1);
+  for (i=0; i< num_pages; i++) {
+    page_table1[i].valid = 0;
+    page_table1[i].pfn = 0
+    page_table2[i].valid = 0;
+    page_table2[i].frame_num = 0;
+  }
+  WriteRegister( REG_PTR0, (RCS421RegVal) &page_table1);
+  WriteRegister( REG_PTR1, (RCS421RegVal) &page_table2);
   /* Map existing physical addresses to same virtual addresses */
+  // TODO
+
   /* Enable virtual memory */
   WriteRegister( REG_VM_ENABLE, (RCS421RegVal) 1);
+
   /* Create an idle process */
-  for (;;){
-    Pause();
-  }
+  process_idle();
 
   // Test
   printf("hello world!");
